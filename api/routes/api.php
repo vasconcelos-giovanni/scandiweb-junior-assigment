a681b4abba985c3f8708dde84178081bf01269a4
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2025-10-13 00:39:41 -0300

    feat(api): add controller and store form request

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,13 +2,13 @@
 declare(strict_types= 1);
 
 use App\Core\Response;
-use App\Services\ProductService;
+use App\Http\Controllers\ProductController;
+
 
 $router->get('/test', function () {
     return Response::json(['message' => 'Hello, World!']);
 });
 
-$router->get('/products', function (ProductService $productService) {
-    $products = $productService->getAllProducts();
-    return Response::json($products);
-});
+$router->get('/products', [ProductController::class, 'index']);
+
+$router->post('/products', [ProductController::class, 'store']);
